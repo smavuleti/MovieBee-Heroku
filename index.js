822da7dfaa8e6b905f6ffef94b321e7a82f55c44
Sruthi Mavuleti <mavuleti160@gmail.com>
2025-01-02 16:02:12 -0800

    Added CORS to allow URL's

--- index.js
+++ index.js
@@ -18,7 +18,20 @@ const passport = require("passport");
 require("./passport");
 
 const cors = require("cors");
-app.use(cors());
+let allowedOrigins = ['https://smavuleti-moviebee-479d2e8d7a6f.herokuapp.com/allMovies','http://localhost:1234/'];
+
+//app.use(cors());
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 const { check, validationResult } = require("express-validator");
 let auth = require("./auth")(app);
 
